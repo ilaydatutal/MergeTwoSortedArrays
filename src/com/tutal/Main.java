9ac5cadbe916ef0341bd51267e3083bdc488c974
İlayda Tutal <tutalilayda@gmail.com>
2019-08-01 15:17:31 +0300

    Method kullanıldı, array'i ekrana bastırma işlemi sadeleştirildi.

--- src/com/tutal/Main.java
+++ src/com/tutal/Main.java
@@ -1,14 +1,27 @@
 package com.tutal;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
         int arr1[] = {1, 2, 3, 4, 5, 6};
         int arr2[] = {5, 6, 7, 8, 9, 10, 11, 66, 78};
-        int result[] = new int[arr1.length + arr2.length];
-        int k = 0, i=0, j=0;
 
+
+
+
+
+
+            System.out.println(Arrays.toString(mergeArrays(arr1, arr2)));
+
+
+    }
+
+    public static int[] mergeArrays(int[] arr1, int[] arr2){
+        int k = 0, i=0, j=0;
+        int result[] = new int[arr1.length + arr2.length];
         while (i < arr1.length ||
                 j < arr2.length) {
 
@@ -35,15 +48,7 @@ public class Main {
             }
             k++;
         }
-
-        for (i = 0; i < result.length; i++) {
-            System.out.println(result[i]);
-        }
-
-    }
-
-    public static int[] mergeArrays(int[] arr1, int[] arr2){
-        return new int[10];
+        return result;
     }
 
 
