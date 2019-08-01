d489c8a68939df168e1410364246f986dc57518d
İlayda Tutal <tutalilayda@gmail.com>
2019-08-01 13:19:16 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,46 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int arr1[] = {1, 2, 3, 4, 5, 6};
+        int arr2[] = {5, 6, 7, 8, 9, 10, 11, 66, 78};
+        int result[] = new int[arr1.length + arr2.length];
+        int k = 0, i=0, j=0;
+
+        while (i < arr1.length ||
+                j < arr2.length) {
+
+            if(i==arr1.length){
+                result[k] = arr2[j];
+                j++;
+                k++;
+                continue;
+            }
+            else if(j==arr2.length){
+                result[k] = arr1[i];
+                i++;
+                k++;
+                continue;
+            }
+
+            if (arr1[i] <= arr2[j]) {
+                result[k] = arr1[i];
+                i++;
+            }
+            else {
+                result[k] = arr2[j];
+                j++;
+            }
+            k++;
+        }
+
+        for (i = 0; i < result.length; i++) {
+            System.out.println(result[i]);
+        }
+
+    }
+
+
+}
